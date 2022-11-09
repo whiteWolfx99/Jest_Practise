765cc7cc989d1cca52f645e74f0cb02f962329a1
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-09 11:53:57 +0300

    linters won't be fixed

--- src/test2.test.js
+++ src/test2.test.js
@@ -3,7 +3,7 @@ const reverseString = require('./test2');
 const string = 'Hevar';
 
 test('reverseString function exists', () => {
-  // if you right anything it will reversed and it will not throw an error
+  // if you write anything it will reversed and it will not throw an error
   const reverse2 = string.split('').reverse().join('');
   expect(reverseString(string)).toBe(reverse2);
   expect((string) => reverseString(string)).toThrow(Error);
