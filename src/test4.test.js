09b0828ff42a456619c03a92aa050d69969aabc9
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-09 12:28:11 +0300

    push

--- src/test4.test.js
+++ src/test4.test.js
@@ -1,8 +1,10 @@
 const cap = require('./test4');
 
 const strings = 'Hevar';
+const strings2 = 'hevar';
 
 test('the capitalize', () => {
   expect(cap(strings)).toBe(strings);
-  expect(() => cap()).toThrow(Error);
+  expect(() => cap(strings2)).toThrow(Error);
 });
+
