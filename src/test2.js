3a0462bbb8c10a1d50836e7cb56b0ad9d828eaea
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-09 11:12:16 +0300

    fixing linters

--- src/test2.js
+++ src/test2.js
@@ -1,14 +1,13 @@
 function reverseString(string) {
-    let reverse = '';
-    for (let i = string.length - 1; i >= 0; i--) {
-        reverse += string[i];
-    }
-    const reverse2 = string.split('').reverse().join('');
-    if (reverse == reverse2) {
-        return reverse;
-    } else {
-        throw new Error('The string is not a Reversed!');
-    }
+  let reverse = '';
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    reverse += string[i];
+  }
+  const reverse2 = string.split('').reverse().join('');
+  if (reverse === reverse2) {
+    return reverse;
+  }
+  throw new Error('The string is not a Reversed!');
 }
 
 module.exports = reverseString;
